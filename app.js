e4390a75f27df4c3974c081cb9dace6080df3be4
Denise Mignoli <denisemignoli@gmail.com>
2025-07-29 23:51:23 -0300

    allow users to add friend names to the list

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let nameList = [];
+
+function addFriend() {
+    let name = document.querySelector('input').value;
+    nameList.push(name);
+    console.log(nameList);
+    displayList();
+    clearInput();
+}
+
+function displayList() {
+    let ul = document.getElementById('friendList');
+    ul.innerHTML = nameList;
+}
+
+function clearInput() {
+    document.querySelector('input').value = '';
+}
