fcaecf3d062a07cc8d368a74f94324a0536cbc6f
Denise Mignoli <denisemignoli@gmail.com>
2025-07-30 14:12:34 -0300

    random friend selection on button

--- app.js
+++ app.js
@@ -1,21 +1,34 @@
-let nameList = [];
+let friendList = [];
 
 function addFriend() {
     let name = document.querySelector('input').value;
-    if (name == ''){
+    if (name != '') {
+        friendList.push(name);
+        console.log(friendList);
+        displayList();
+        clearInput();
+    } else {
         alert('Por favor, informe um nome válido!')
     }
-    nameList.push(name);
-    console.log(nameList);
-    displayList();
-    clearInput();
 }
 
 function displayList() {
     let ul = document.getElementById('friendList');
-    ul.innerHTML = nameList;
+    ul.innerHTML = friendList;
 }
 
 function clearInput() {
     document.querySelector('input').value = '';
+}
+
+function pickRandomFriend() {
+    let randomIndex = Math.floor(Math.random() * friendList.length);
+    let selectedFriend = friendList[randomIndex];
+    displaySelectedFriend(selectedFriend);
+    console.log(selectedFriend);
+}
+
+function displaySelectedFriend(selectedFriend) {
+    let result = document.getElementById('result');
+    result.innerHTML = selectedFriend;
 }
